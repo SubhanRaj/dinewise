f1d8e3e2f9b327175c9e71eef85f3441a37521ff
Subhan Raj <shubhanraj2002@gmail.com>
2024-04-22 10:00:48 +0530

    Update branding and copyright information

--- resources/views/components/frontend.blade.php
+++ resources/views/components/frontend.blade.php
@@ -65,13 +65,13 @@
             <div class="row d-flex align-items-center">
                 <div class="col-lg-6 text-lg-left text-center">
                     <div class="copyright">
-                        &copy; Copyright <strong>Vesperr</strong>. All Rights Reserved
+                        &copy; Copyright <strong>Dine Wise</strong>. All Rights Reserved
                     </div>
                     <div class="credits">
                         <!-- All the links in the footer should remain intact. -->
                         <!-- You can delete the links only if you purchased the pro version. -->
                         <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
+                        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/Dine Wise-free-bootstrap-template/ -->
                         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
                     </div>
                 </div>
