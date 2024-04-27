b34a90a8f4978f593463a2e36853e07e891c1ee5
Subhan Raj <shubhanraj2002@gmail.com>
2024-04-27 09:44:59 +0530

    Dynamic Username & role

--- resources/views/reception/header.blade.php
+++ resources/views/reception/header.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-     <meta http-equiv='Expires' content='0'>
+    <meta http-equiv='Expires' content='0'>
     <meta http-equiv='Pragma' content='no-cache'>
     <meta http-equiv='Cache-Control' content='no-cache'>
     <!--favicon-->
@@ -15,8 +15,7 @@
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/png" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
 
-    <link href="{{ asset('admin-assets/assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}"
-        rel="stylesheet">
+    <link href="{{ asset('admin-assets/assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
     <link href="{{ asset('admin-assets/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
 
     <link href="{{ asset('admin-assets/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
@@ -44,10 +43,8 @@
         <div class="sidebar-wrapper" data-simplebar="true">
             <div class="sidebar-header">
                 <div>
-                    <img src="{{ asset('admin-assets/assets/images/logo.png') }}" class="logo-icon logo-big"
-                        alt="logo icon">
-                    <img src="{{ asset('admin-assets/assets/images/logo-sm.png') }}" class="logo-icon d-none logo-sm"
-                        alt="logo icon">
+                    <img src="{{ asset('admin-assets/assets/images/logo.png') }}" class="logo-icon logo-big" alt="logo icon">
+                    <img src="{{ asset('admin-assets/assets/images/logo-sm.png') }}" class="logo-icon d-none logo-sm" alt="logo icon">
                 </div>
 
                 <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
@@ -70,19 +67,15 @@
                         <div class="menu-title">Inquiry</div>
                     </a>
                     <ul>
-                        <li> <a href="{{ route('reception.addInquiryIndex') }}"><i
-                                    class="bx bx-right-arrow-alt"></i>Add
+                        <li> <a href="{{ route('reception.addInquiryIndex') }}"><i class="bx bx-right-arrow-alt"></i>Add
                                 Inquiry</a>
                         </li>
-                        <li> <a href="{{ route('reception.showInquiry') }}"><i
-                                    class="bx bx-right-arrow-alt"></i>Inquiry
+                        <li> <a href="{{ route('reception.showInquiry') }}"><i class="bx bx-right-arrow-alt"></i>Inquiry
                                 Details</a>
                         </li>
-                        <li> <a href="{{ route('reception.showFollowUpInquiry') }}"><i
-                                    class="bx bx-right-arrow-alt"></i>Follow Up</a>
+                        <li> <a href="{{ route('reception.showFollowUpInquiry') }}"><i class="bx bx-right-arrow-alt"></i>Follow Up</a>
                         </li>
-                        <li> <a href="{{ route('reception.showSuccessfullInquiry') }}"><i
-                                    class="bx bx-right-arrow-alt"></i>Successfull Inquiry</a>
+                        <li> <a href="{{ route('reception.showSuccessfullInquiry') }}"><i class="bx bx-right-arrow-alt"></i>Successfull Inquiry</a>
                         </li>
                     </ul>
                 </li>
@@ -93,12 +86,10 @@
                         <div class="menu-title">Reception</div>
                     </a>
                     <ul>
-                        <li> <a href="{{ route('reception.addReceptionIndex') }}"><i
-                                    class="bx bx-right-arrow-alt"></i>Add
+                        <li> <a href="{{ route('reception.addReceptionIndex') }}"><i class="bx bx-right-arrow-alt"></i>Add
                                 Logs</a>
                         </li>
-                        <li> <a href="{{ route('reception.showReceptionIndex') }}"><i
-                                    class="bx bx-right-arrow-alt"></i>Logs Details</a>
+                        <li> <a href="{{ route('reception.showReceptionIndex') }}"><i class="bx bx-right-arrow-alt"></i>Logs Details</a>
                         </li>
                     </ul>
                 </li>
@@ -116,33 +107,29 @@
                     <div class="top-menu ms-auto">
                         <ul class="navbar-nav align-items-center">
                             <li class="nav-item">
-                                <a class="nav-link" href="#" data-bs-toggle="tooltip"
-                                    data-bs-title="Full Screen Mode" data-bs-placement="auto" onclick="fullScreen()"
-                                    id="fullscreen-btn"> <i class='bx bx-fullscreen' id="fullscreen-icon"></i>
+                                <a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-title="Full Screen Mode" data-bs-placement="auto" onclick="fullScreen()" id="fullscreen-btn"> <i class='bx bx-fullscreen' id="fullscreen-icon"></i>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link theme-change" href="#" id="theme-change-btn"
-                                    onclick="changeTheme()" data-bs-toggle="tooltip"
-                                    data-bs-title="Switch to dark mood" data-bs-placement="auto"> <i
-                                        class='bx bx-moon'></i>
+                                <a class="nav-link theme-change" href="#" id="theme-change-btn" onclick="changeTheme()" data-bs-toggle="tooltip" data-bs-title="Switch to dark mood" data-bs-placement="auto"> <i class='bx bx-moon'></i>
                                 </a>
                             </li>
                         </ul>
                     </div>
                     <div class="user-box dropdown">
-                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret"
-                            href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="{{ asset('admin-assets/assets/images/digi-user.png') }}" class="user-img"
-                                alt="user avatar">
+                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="{{ asset('admin-assets/assets/images/digi-user.png') }}" class="user-img" alt="user avatar">
                             <div class="user-info ps-3">
-                                <p class="user-name mb-0">Dine Wise</p>
-                                <p class="designattion mb-0">Digital Marketing Agency</p>
+                                <p class="user-name mb-0">
+                                    {{ session('username') }}
+                                </p>
+                                <p class="designattion mb-0">
+                                    {{ session('role') }}
+                                </p>
                             </div>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="{{ route('reception.logout') }}"><i
-                                        class='bx bx-log-out-circle'></i><span>Logout</span></a>
+                            <li><a class="dropdown-item" href="{{ route('reception.logout') }}"><i class='bx bx-log-out-circle'></i><span>Logout</span></a>
                             </li>
                         </ul>
                     </div>
